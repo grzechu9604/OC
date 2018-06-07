5da691990fc694821198c6f9e318aa7d77946166
Grzegorz Wilczyński <grzegorz.wilczynski@student.put.poznan.pl>
2018-06-07 22:16:55 +0200

    Optymalizacja obliczania odległości między punktami

--- source.py
+++ source.py
@@ -2,16 +2,15 @@ import numpy as np
 
 
 def calculate_distance(point_a, point_b):
-    distance: float = 0
-    for i in range(len(point_a)):
-        distance += (point_a[i] - point_b[i]) ** 2
+    diff_vector = np.absolute(point_a - point_b)
+    distance = diff_vector @ diff_vector.T
     distance **= 1/2
     return distance
 
 
 def read_distance_matrix_from_file(path, elements_separator, lines_separator):
     file = open(path, 'r')
-    lines = [line.replace("\n","").split(lines_separator) for line in file][0]
+    lines = [line.replace("\n", "").split(lines_separator) for line in file][0]
     strings_matrix = [line.split(elements_separator) for line in lines]
     matrix = [[float(element) for element in line] for line in strings_matrix]
     return np.array(matrix)
@@ -24,6 +23,7 @@ def main():
     point_a = np.array([1, 1, 1])
     point_b = np.array([0, 0, 0])
     print(calculate_distance(point_a, point_b))
+    print(calculate_distance(point_b, point_a))
     print(calculate_distance(2 * point_a, point_b))
     print(calculate_distance(10 * point_a, point_b))
     print(calculate_distance(3 * point_a, point_b))
