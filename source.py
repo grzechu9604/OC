6638d5faf06cf49612890bff0234ba7e84591b66
Grzegorz Wilczyński <grzegorz.wilczynski@student.put.poznan.pl>
2018-06-07 23:10:58 +0200

    MAcierzowe obliczanie odległości

--- source.py
+++ source.py
@@ -1,10 +1,21 @@
 import numpy as np
 
 
+def calculate_distance_to_all_points_by_point_index(i, matrix):
+    return calculate_distance_to_all_points(matrix[i], matrix)
+
+
+def calculate_distance_to_all_points(point, matrix):
+    diff_matrix = matrix - point
+    diff_vector = []
+    for i in range(len(diff_matrix)):
+        diff_vector.append((diff_matrix[i] @ diff_matrix[i].T) ** (1/2))
+    return np.array(diff_vector)
+
+
 def calculate_distance(point_a, point_b):
     diff_vector = np.absolute(point_a - point_b)
-    distance = diff_vector @ diff_vector.T
-    distance **= 1/2
+    distance = (diff_vector @ diff_vector.T) ** (1/2)
     return distance
 
 
@@ -27,8 +38,10 @@ def main():
     print(calculate_distance(2 * point_a, point_b))
     print(calculate_distance(10 * point_a, point_b))
     print(calculate_distance(3 * point_a, point_b))
+    arr = np.array([point_a, point_b, 2 * point_a, 10 * point_a, 3 * point_a])
+    print(calculate_distance_to_all_points(point_b, arr))
+    print(calculate_distance_to_all_points_by_point_index(1, arr))
 
 
 if __name__ == '__main__':
     main()
-
